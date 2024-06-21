58caf5d26663c675fa0a1dffb92d4f5387ead368
pai019 <paint0208@gmail.com>
2024-06-21 22:31:47 +0900

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('#menu ul li a').forEach(link => {
+
+    link.addEventListener('click', function(event) {
+        // 모든 메뉴 항목에서 'active' 클래스 제거
+        document.querySelectorAll('#menu ul li a').forEach(item => {
+            item.classList.remove('active');
+        });
+        // 클릭한 메뉴 항목에만 'active' 클래스 추가
+        this.classList.add('active');
+    });
+});
